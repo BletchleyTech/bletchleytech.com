9a905ad5d31d5e341bd9807f1b69a7c62a7804dc
Jorge Gallego <gallegojorge908@gmail.com>
2021-04-28 22:06:14 -0500

    Improved logic

--- services/services.js
+++ services/services.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(express.static(`${__dirname}/../static`));
+
 router.get("/", (req, res) => {
     res.sendFile(`${__dirname}/services.html`);
 });
