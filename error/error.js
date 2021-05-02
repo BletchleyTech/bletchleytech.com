91cb32044f53091078c100ca1808cbf489a95ff6
Jorge Gallego <gallegojorge908@gmail.com>
2021-05-02 15:14:29 -0500

    Updated error response logic

--- error/error.js
+++ error/error.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 router.use(express.static(`${__dirname}/../static`));
 
-router.use("/", (req, res) => {
-    res.statusCode = 404;
-    res.statusMessage = 'Not Found';
-    res.sendFile(`${__dirname}/error.html`);
+router.use((req, res) => {
+    res.status(404).sendFile(`${__dirname}/404.html`);
+});
+
+router.use((req, res) => {
+    res.status(500).send("Internal Server Error");
+    // res.status(500).sendFile(`${__dirname}/500.html`);
 });
 
 module.exports = router;
