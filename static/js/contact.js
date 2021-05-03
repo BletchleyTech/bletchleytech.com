e5ed774b9e79b4a69eef4337f7899bffcae513de
Jorge Gallego <gallegojorge908@gmail.com>
2021-05-03 07:26:01 -0500

    Added utilities

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,1 +1,15 @@
 const text = 'Contact Us';
+
+if (window.location.href.includes("?"))
+{
+    const service = {"web": 0, "app": 1};
+    let query = window.location.href.split("?").splice(1)[0].split("&")[0].split("=")[1];
+    if (query.includes("#"))
+    {
+        document.querySelector("select").selectedIndex = service[query.split("#")[0]];
+    }
+    else
+    {
+        document.querySelector("select").selectedIndex = service[query];
+    }
+}
