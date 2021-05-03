9b2ee496251fc6837bfd480255fced3f5036e62d
Jorge Gallego <gallegojorge908@gmail.com>
2021-05-03 07:27:50 -0500

    Commented links to unused routes

--- services/services.js
+++ services/services.js
@@ -16,16 +16,16 @@ router.get("/app", (req, res) => {
     res.sendFile(path.join(__dirname, "app.html"));
 });
 
-router.get("/game", (req, res) => {
-    res.send("<h1>Game Development</h1>");
-});
+// router.get("/game", (req, res) => {
+//     res.send("<h1>Game Development</h1>");
+// });
 
-router.get("/enterprise", (req, res) => {
-    res.send("<h1>Enterprise App Development</h1>");
-});
+// router.get("/enterprise", (req, res) => {
+//     res.send("<h1>Enterprise App Development</h1>");
+// });
 
-router.get("/vr", (req, res) => {
-    res.send("<h1>Virtual Reality Development</h1>");
-});
+// router.get("/vr", (req, res) => {
+//     res.send("<h1>Virtual Reality Development</h1>");
+// });
 
 module.exports = router;
