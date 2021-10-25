b6234faa5abef6c4998c8810fff82389fb66bfbc
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-24 19:44:10 -0500

    Relocated routing files

--- routing/error.js
+++ routing/error.js
@@ -3,15 +3,24 @@ const router = express.Router();
 
 const name = "Bletchley Technological Solutions Inc.";
 
+router.get("/error", (req, res) => {
+    res.status(500).render("error", {
+        title: `Internal Server Error - ${name}`,
+        path: req.path
+    });
+});
+
 router.use("*/", (req, res) => {
     res.status(404).render("404", {
-        title: `Page Not Found - ${name}`
+        title: `Page Not Found - ${name}`,
+        path: req.path
     });
 });
 
 router.use((error, req, res, next) => {
     res.status(500).render("500", {
-        title: `Internal Server Error - ${name}`
+        title: `Internal Server Error - ${name}`,
+        path: req.path
     });
     console.error(error);
 });
