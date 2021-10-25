9ee6c4033047cb3b1c891ef166c375861d5c4031
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-25 07:29:13 -0500

    Refactored routes and implemented CORS

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
+const routes = require("./routing/routes");
 const about = require("./routing/about");
 const services = require("./routing/services");
 const contact = require("./routing/contact");
@@ -12,29 +14,16 @@ const port = 3012;
 connect('mongodb://localhost:27017/bletchley');
 
 app.use(express.static(path.join(__dirname, "static")));
+app.use(cors());
 app.set("view engine", "pug");
 
-const name = "Bletchley Technological Solutions Inc.";
-
-app.get("/", (req, res) => {
-    res.render("index", {
-        title: name, 
-        path: "/"
-    });
-});
+app.use(routes);
 
 app.use("/about", about);
 
 app.use("/services", services);
 
 app.use("/contact", contact);
-
-app.get("/faq", (req, res) => {
-    res.render("faq", {
-        title: `Frequently Asked Questions - ${name}`,
-        path: "/faq"
-    });
-});
 
 app.use(files);
 
