895ca81fbc381de8b738813ea9af9b82434ec9ac
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-25 07:29:26 -0500

    Refactored routes

---
+++ routing/routes.js
@@ -0,0 +1,20 @@
+const { Router } = require("express");
+const router = Router();
+
+const name = "Bletchley Technological Solutions Inc.";
+
+router.get("/", (req, res) => {
+    res.render("index", {
+        title: name, 
+        path: "/"
+    });
+});
+
+router.get("/faq", (req, res) => {
+    res.render("faq", {
+        title: `Frequently Asked Questions - ${name}`,
+        path: "/faq"
+    });
+});
+
+module.exports = router;
