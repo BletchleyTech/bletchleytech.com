6145cc2ac047409c28fcfe37ed2ffc18367097d8
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-25 11:16:09 -0500

    Created Client model

---
+++ models/client.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const clientSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    service: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true
+    },
+    logo: {
+        type: String,
+        required: true
+    }
+});
+
+const Client = new model('Client', clientSchema);
+
+module.exports = Client;
