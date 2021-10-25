110f2b17746c25fcdf0054910d4ce7bdd0a01db2
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-25 14:41:23 -0500

    Implemented Client model

--- routing/routes.js
+++ routing/routes.js
@@ -1,18 +1,20 @@
 const { Router } = require("express");
 const Testimonial = require("./../models/testimonial");
+const Client = require("./../models/client");
 const router = Router();
 
 const name = "Bletchley Technological Solutions Inc.";
 
 router.get("/", (req, res) => {
-    Testimonial.find({}, 'quote name company', {limit: 5}, (err, testimonials) => {
+    Testimonial.find({}, 'quote name company', (err, testimonials) => {
         if (err) {
             console.error(err);
             res.redirect("/error");
         } else {
+            testimonials.reverse().slice(0, 5);
             res.render("index", {
                 title: name, 
-                path: "/",
+                path: req.path,
                 testimonials
             });
         }
@@ -22,7 +24,23 @@ router.get("/", (req, res) => {
 router.get("/faq", (req, res) => {
     res.render("faq", {
         title: `Frequently Asked Questions - ${name}`,
-        path: "/faq"
+        path: req.path
+    });
+});
+
+router.get("/clients", (req, res) => {
+    Client.find({}, 'name service rating logo', (err, clients) => {
+        if (err) {
+            console.error(err);
+            res.redirect("/error");
+        } else {
+            clients.reverse();
+            res.render("clients", {
+                title: `Our Clients - ${name}`,
+                path: req.path,
+                clients
+            });
+        }
     });
 });
 
