775dbf18511f10410c6fced2c164139af2eeaefb
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-26 09:11:21 -0500

    Created 'Admin' model

---
+++ models/admin.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const adminSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pass: {
+        type: String,
+        required: true
+    }
+});
+
+adminSchema.plugin(encrypt, {secret: process.env.ADMIN, encryptedFields: ['pass']});
+
+const Admin = new model('Admin', adminSchema);
+
+module.exports = Admin;
