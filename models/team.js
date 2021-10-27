5a001cd0f8b1c023b8ed4a3f09a51e93a6085665
Jorge Gallego <gallegojorge908@gmail.com>
2021-10-27 07:05:41 -0500

    Created Member model

---
+++ models/team.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const memberSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    }
+});
+
+const Member = new model('Member', memberSchema);
+
+module.exports = Member;
