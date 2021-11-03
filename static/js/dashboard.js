fee838678c90aba5556e7c7efb4a8ba409106228
Jorge Gallego <gallegojorge908@gmail.com>
2021-11-03 11:31:35 -0500

    Added functionality

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -4,7 +4,11 @@ document.querySelectorAll("aside.row #actions button").forEach(button => {
         document.querySelectorAll(`aside.row #actions button:not(.${id})`).forEach(notButton => {
             notButton.classList.remove("active");
         });
-        button.classList.add("active");
+        if (button.classList.contains("active")) {
+            button.classList.remove("active");
+        } else {
+            button.classList.add("active");
+        }
         document.querySelectorAll(`aside.row #forms form:not(#${id})`).forEach(form => {
             form.classList.remove("show");
         });
